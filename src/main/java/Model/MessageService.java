bad58399a7c1f55d1e3273bce4ef034486d3e267
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 10:45:16 +0300

    MessageService 2 commit Amir

--- src/main/java/Model/MessageService.java
+++ src/main/java/Model/MessageService.java
@@ -1,4 +1,17 @@
 package Model;
 
 public class MessageService {
+    private Message[] messages;
+    public Message send(User sender, User recipient, String message) {
+        Message result = new Message(sender, recipient, message, "2024-12-07 10:25");
+        return result;
+    }
+    public Message[] createMessages(User[] users){
+        messages = new Message[4];
+        messages[0] = send(users[0], users[1], "First message");
+        messages[1] = send(users[1], users[2], "Second message");
+        messages[2] = send(users[2], users[3], "Third message");
+        messages[3] = send(users[3], users[4], "Fourth message");
+        return messages;
+    }
 }
