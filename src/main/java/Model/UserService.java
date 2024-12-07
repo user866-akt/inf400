a43f96b2e01d970bcc7701ccaad5c8b12a34f363
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 11:27:34 +0300

    repaired

--- src/main/java/Model/UserService.java
+++ src/main/java/Model/UserService.java
@@ -5,13 +5,8 @@ public class UserService {
     private static User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
     private static User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
 
-<<<<<<< HEAD
     public static String[] getUsers() {
         String[] users = {user1.getName(), user2.getName(), user3.getName()};
-=======
-    public static User[] getUsers() {
-        User[] users = {user1, user2, user3};
->>>>>>> 8989b9997582b09569e45c64c5bdc27ae7a6ca34
         return users;
     }
 }
