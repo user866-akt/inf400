2b04666274d4fcb4f3121cabfdf4b39c8c16fb39
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 09:52:27 +0300

    userservice 2 commit Amir

--- src/main/java/Model/UserService.java
+++ src/main/java/Model/UserService.java
@@ -1,5 +1,7 @@
 package Model;
 
 public class UserService {
-
+    User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
+    User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
+    User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
 }
