b8088321a516bafc0ff341583df091f2676e2803
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 10:50:48 +0300

    UserService 2 commit

--- src/main/java/Model/UserService.java
+++ src/main/java/Model/UserService.java
@@ -1,7 +1,19 @@
 package Model;
 
 public class UserService {
-    User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
-    User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
-    User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
+    private User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
+    private User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
+    private User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public User getUser3() {
+        return user3;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
 }
