60802958665ad52922565645c6ed225c66ff7ed5
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 11:07:32 +0300

    last commit

--- src/main/java/Model/MessageService.java
+++ src/main/java/Model/MessageService.java
@@ -1,13 +1,12 @@
 package Model;
 
 public class MessageService {
-    private Message[] messages;
-    public Message send(User sender, User recipient, String message) {
+    public static Message send(User sender, User recipient, String message) {
         Message result = new Message(sender, recipient, message, "2024-12-07 10:25");
         return result;
     }
-    public Message[] createMessages(User[] users){
-        messages = new Message[4];
+    public static Message[] createMessages(User[] users){
+        Message[] messages = new Message[4];
         messages[0] = send(users[0], users[1], "First message");
         messages[1] = send(users[1], users[2], "Second message");
         messages[2] = send(users[2], users[0], "Third message");
--- src/main/java/Model/UserService.java
+++ src/main/java/Model/UserService.java
@@ -4,16 +4,4 @@ public class UserService {
     private User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
     private User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
     private User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
-
-    public User getUser1() {
-        return user1;
-    }
-
-    public User getUser3() {
-        return user3;
-    }
-
-    public User getUser2() {
-        return user2;
-    }
 }
