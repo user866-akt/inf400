ee42aff44e82e58265529e14e816a3d6a14586b2
Amir Khubeev <hubeevamir@gmail.com>
2024-12-07 11:21:05 +0300

    final commit Amir

--- src/main/java/Model/UserService.java
+++ src/main/java/Model/UserService.java
@@ -1,12 +1,12 @@
 package Model;
 
 public class UserService {
-    private User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
-    private User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
-    private User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
+    private static User user1 = new User("Amir", "Amir Khubeev", "878787878", "42r2gfg24");
+    private static User user2 = new User("Vladislav", "Vladislav Stashkov", "234794894", "3r21gg9");
+    private static User user3 = new User("Arina", "Arina Amirova", "3716418547", "ctg43f323f");
 
-    public User[] getUsers() {
-        User[] users = {user1, user2, user3};
+    public static String[] getUsers() {
+        String[] users = {user1.getName(), user2.getName(), user3.getName()};
         return users;
     }
 }
