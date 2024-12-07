9f0da318cc2b87e0b34e2c151d0e749159423e29
arina <amirovaarina06@yandex.ru>
2024-12-07 10:35:00 +0300

    second commit arina

--- src/main/java/Model/Message.java
+++ src/main/java/Model/Message.java
@@ -2,15 +2,41 @@ package Model;
 
 public class Message {
 
-    private String sender;
-    private String recipient;
+    private User sender;
+    private User recipient;
     private String text;
     private String dateTime;
 
-    public Message(String sender, String recipient,String text, String dateTime) {
+    public Message(User sender, User recipient,String text, String dateTime) {
         this.sender = sender;
         this.recipient = recipient;
         this.text = text;
+        this.dateTime = dateTime;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+    public User getRecipient() {
+        return recipient;
+    }
+    public String getText() {
+        return text;
+    }
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
     }
 
